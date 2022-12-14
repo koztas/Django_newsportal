e5645c86772bfc561f5f0f184cdb71aeaa9667ed
Konstantin <106991417+koztas@users.noreply.github.com>
2022-12-14 14:34:12 +0300

    Update settings.py

--- NewsPortal/NewsPaper/NewsPaper/settings.py
+++ NewsPortal/NewsPaper/NewsPaper/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'NewsPaper.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'news/template/news')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -127,7 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'news/static/news')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = []
 
 # Default primary key field type
