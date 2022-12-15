5fa6a88faaf713726fe5a2f79d266ab958ec1f39
Konstantin <106991417+koztas@users.noreply.github.com>
2022-12-15 13:42:34 +0300

    Update views.py

--- NewsPortal/NewsPaper/news/views.py
+++ NewsPortal/NewsPaper/news/views.py
@@ -1,9 +1,12 @@
-from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import Group
+from django.shortcuts import redirect, get_object_or_404, render
 from django.urls import reverse_lazy
 from django.views.generic import (
     ListView, DetailView, CreateView, UpdateView, DeleteView,
 )
-from .models import Post
+from .models import Post, Category
 from .filters import PostFilter
 from .forms import PostForm
 from django.http import HttpResponse
@@ -11,14 +14,36 @@ from django.http import HttpResponse
 
 class PostList(ListView):
     model = Post
+    ordering = 'title'
     context_object_name = 'posts'
     template_name = 'posts.html'
+    paginate_by = 5
+    
+    # def get_context_data(self, **kwargs):
+    #
+    #     context = super().get_context_data(**kwargs)
+    #     context['time_now'] = datetime.utcnow()
+    #     context['next_news'] = "Горячие новости - позже"
+    #     return context
 
 
 class PostDetail(DetailView):
     model = Post
     context_object_name = 'post'
     template_name = 'post.html'
+
+    
+class PostSearch(ListView):
+    model = Post
+    template_name = 'post_search.html'
+    context_object_name = 'search'
+    paginate_by = 2
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filter'] = PostFilter(self.request.GET, queryset=self.get_queryset())
+        context['is_not_author'] = not self.request.user.groups.filter(name='authors').exists()
+        return context
 
 
 class PostCreate(PermissionRequiredMixin, CreateView):
@@ -40,3 +65,24 @@ class PostDelete(PermissionRequiredMixin, DeleteView):
     model = Post
     template_name = 'post_delete.html'
     success_url = reverse_lazy('post_list')
+    queryset = Post.objects.all()
+
+
+class CategoryListView(ListView):
+    model = Post
+    template_name = 'category_list.html'
+    context_object_name = 'category_news_list'
+
+    def get_queryset(self):
+        self.category = get_object_or_404(Category, id=self.kwargs['pk'])
+        queryset = Post.objects.filter(category=self.category).order_by('-date')
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['is_not_subscriber'] = self.request.user not in self.category.subscribers.all()
+        context['category'] = self.category
+        return context
+
+# def index(request):
+#     return render(request, 'index.html')
