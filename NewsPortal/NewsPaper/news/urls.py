29c88a75b9919593103a32373e6ff91e80540f86
Konstantin <106991417+koztas@users.noreply.github.com>
2022-12-15 13:44:59 +0300

    Update urls.py

--- NewsPortal/NewsPaper/news/urls.py
+++ NewsPortal/NewsPaper/news/urls.py
@@ -1,10 +1,14 @@
+from django.contrib import admin
 from django.urls import path
-from .views import PostDetail, PostList, PostCreate, PostUpdate, PostDelete
+from .views import PostDetail, PostList, PostCreate, PostUpdate, PostDelete, \
+    CategoryListView
 
 urlpatterns = [
     path('', PostList.as_view(), name='post_list'),
     path('<int:pk>', PostDetail.as_view(), name='post_detail'),
+    path('search/', PostSearch.as_view(), name='post_search'),
     path('create/', PostCreate.as_view(), name='post_create'),
     path('<int:pk>/update', PostUpdate.as_view(), name='post_update'),
     path('<int:pk>/delete', PostDelete.as_view(), name='post_delete'),
+    path('categories/<int:pk>', CategoryListView.as_view(), name='category_list'),
 ]
